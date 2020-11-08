76c714d4b735dc1a75d60221a8b882dc6e667a72
Sean Downey <sean@considerweb.com>
2020-11-08 14:03:55 +0000

    Adding a link to the order for internal emails

--- src/resources/views/store/mail/orders/new.blade.php
+++ src/resources/views/store/mail/orders/new.blade.php
@@ -9,4 +9,7 @@ An order has been placed on {{ config('app.name') }}.
 
 @include('seandowney::mail.orders.partials.order_summary')
 
+@component('mail::button', ['url' => secure_url(config('backpack.base.route_prefix', 'admin').'/'.config('seandowney.storecrud.route_prefix', 'store').'/order/'.$order->id.'/edit'), 'color' => 'red'])
+View Order
+@endcomponent
 @endcomponent
